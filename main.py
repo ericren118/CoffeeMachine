acf3c7c8708a51b34ab57e019486fdc8b6d1cd75
Eric Ren <ericren118@gmail.com>
2022-05-14 14:38:07 +1000

    Coffee Machine - Initial Commit2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 #
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 MENU = {
-    "espresso": {
+    "expresso": {
         "ingredients": {
             "water": 50,
             "coffee": 18,
@@ -63,62 +63,71 @@ def check_resources(drink, current_resources):
     """Check if there is enough resources for the order"""
     drink_standard = MENU[drink]["ingredients"]
     warning_message = ""
-    if drink == MENU[drink]:
+    if drink in MENU:
         if current_resources["water"] < drink_standard["water"]:
             warning_message = f"Not enough water for a {drink} \n"
-        if current_resources["milk"] < drink_standard["milk"]:
-            warning_message += f"Not enough milk for a {drink} \n"
+        if drink != "expresso":
+            if current_resources["milk"] < drink_standard["milk"]:
+                warning_message += f"Not enough milk for a {drink} \n"
         if current_resources["coffee"] < drink_standard["coffee"]:
             warning_message += f"Not enough coffee beans for a {drink}"
-    return warning_message
+    else:
+        warning_message = "Please check your drink choice!"
+    if warning_message != "":
+        print(warning_message)
+        resource_available = False
+    else:
+        resource_available = True
+    return resource_available
 
 
-def check_transaction():
-
-    pass
+def make_coffee(ordered_drink, current_resources):
+    drink = MENU[ordered_drink]["ingredients"]
+    current_resources["water"] -= drink["water"]
+    current_resources["coffee"] -= drink["coffee"]
+    if ordered_drink != "expresso":
+        current_resources["milk"] -= drink["milk"]
+    print("Here is your ☕. Enjoy!")
 
 
 def start_coffee_machine():
     machine_on = True
 
     while machine_on:
-        choice = input("What would you like? expresso/latte/cappuccino: ")
-    pass
+        drink = input("What would you like? expresso/latte/cappuccino: ").lower()
+        if drink == "report":
+            print_report(resources, profit)
+        elif drink == "off":
+            print("Turning off...")
+            machine_on = False
+        else:
+            if check_resources(drink, resources):
+                if payment(drink, profit):
+                    make_coffee(drink, resources)
 
 
-def payment(quarter_num, dime_num, nickle_num, pennie_num, drink, profit):
+def payment(drink, current_profit):
+    quarter_num = float(input("How many quarters? "))
+    dime_num = float(input("How many dimes? "))
+    nickle_num = float(input("How many nickles? "))
+    pennie_num = float(input("How many pennies? "))
+
     total_payment = QUARTERS * quarter_num + DIMES * dime_num + NICKLE * nickle_num + PENNIE * pennie_num
     drink_price = MENU[drink]["cost"]
+
     if total_payment < drink_price:
         print(f"Sorry, that's not enough money for a ${drink_price} {drink} \n"
               f"Your payment of {total_payment} has been refunded.")
-        return start_coffee_machine()
+        payment_successful = False
+        return payment_successful
     else:
         if total_payment > drink_price:
             change = total_payment - drink_price
             print(f"Here is ${change} dollars in change.")
-        profit += drink_price
+        current_profit += drink_price
+        profit = current_profit
+        payment_successful = True
+        return payment_successful
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+start_coffee_machine()
